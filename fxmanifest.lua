ac2c72222ad6f333ad4033e11269496900024f81
xMaddMackx <82854182+xMaddMackx@users.noreply.github.com>
2023-02-14 08:52:47 -0500

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -6,7 +6,7 @@ lua54 {'yes'}
 name 'QB Lets Cook!'
 author 'XMMX SCRIPTS'
 description 'Extensive Ingredient Cooking Script with Props as Items'
-version '3.0.6'
+version '3.5.0'
 
 client_scripts { 
     'client/*.lua' 
